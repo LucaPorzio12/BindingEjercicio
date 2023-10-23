10c810d3e646d4dc14d0be70834a98e0d3e51ea6
lucaporzio <luca.porzio@cieep.com>
2023-10-23 11:25:13 +0200

    Primera Parte

--- app/src/main/java/luca/porzio/bindingejercicio/MainActivity.java
+++ app/src/main/java/luca/porzio/bindingejercicio/MainActivity.java
@@ -1,8 +1,13 @@
 package luca.porzio.bindingejercicio;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
@@ -10,6 +15,7 @@ import android.view.View;
 import luca.porzio.bindingejercicio.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    private ActivityResultLauncher<Intent> launcherAddInmueble;
 
     private ActivityMainBinding binding;
 
@@ -20,11 +26,24 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        inicializarLaunchers();
+
         binding.fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+             launcherAddInmueble.launch(new Intent(MainActivity.this, AddInmuebleActivity.class));
             }
         });
+    }
+
+    private void inicializarLaunchers() {
+        launcherAddInmueble = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult o) {
+                        //RESULTADO DE VOLVER DE LA ACTIVIDAD ADD INMUEBLE
+                    }
+                }
+        );
     }
 }
